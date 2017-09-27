70002dec69639570bda2dc5d1eea4ae57c9424b8
Michael Yee <michaelqyee@gmail.com>
2017-09-26 18:57:57 -0600

    added comments to game agent

--- game_agent.py
+++ game_agent.py
@@ -34,15 +34,34 @@ def custom_score(game, player):
     float
         The heuristic value of the current game state to the specified player.
     """
+
+    # if a move causes the player to lose, the score should be the worst it can be
     if game.is_loser(player):
         return float("-inf")
 
+    # if a move causes the player to win, the score should be the worst it can be
     if game.is_winner(player):
         return float("inf")
 
-    own_moves = len(game.get_legal_moves(player))
-    opp_moves = len(game.get_legal_moves(game.get_opponent(player)))
-    return float(own_moves - opp_moves)
+    # define lists of moves for each player
+    own_moves = game.get_legal_moves(player)
+    opp_moves = game.get_legal_moves(game.get_opponent(player))
+
+    own_moves_score = 0
+    opp_moves_score = 0
+
+    # iterate over the movelist for the active player
+    for move in own_moves:
+        # sum up all of the possible moves resulting from executing the current move
+        own_moves_score += len(game._Board__get_moves(move))
+
+    # iterate over the movelist for the active player
+    for move in opp_moves:
+        # sum up all of the possible moves resulting from executing the current move
+        opp_moves_score += len(game._Board__get_moves(move))
+
+    # return the difference between the possible moves unlocked by the current move
+    return float(own_moves_score - opp_moves_score)
 
 
 def custom_score_2(game, player):
@@ -67,15 +86,29 @@ def custom_score_2(game, player):
     float
         The heuristic value of the current game state to the specified player.
     """
+
+    # if a move causes the player to lose, the score should be the worst it can be
     if game.is_loser(player):
         return float("-inf")
 
+    # if a move causes the player to win, the score should be the worst it can be
     if game.is_winner(player):
         return float("inf")
 
+    # defining the beginning of the game as 25% blank spaces, check if we've passed the beginning of the game
+    if (len(game.get_blank_spaces()) < 0.75 * game.height * game.width):
+        # define the set of moves for each player
+        own_moves_set = set(game.get_legal_moves(player))
+        opp_moves_set = set(game.get_legal_moves(game.get_opponent(player)))
+        # favor moves that offer the ability to block the opponent by selecting moves that lead to move overlap with the opponent
+        return float(len(own_moves_set.intersection(opp_moves_set)))
+
+    # otherwise, if still at the beginning of the game, count moves for both players
     own_moves = len(game.get_legal_moves(player))
     opp_moves = len(game.get_legal_moves(game.get_opponent(player)))
-    return float(own_moves - 2 * opp_moves)
+
+    # return difference between number of moves available, weighting more importance on the active player's moves to create a solid foundation
+    return float(3 * own_moves - opp_moves)
 
 
 def custom_score_3(game, player):
@@ -100,14 +133,20 @@ def custom_score_3(game, player):
     float
         The heuristic value of the current game state to the specified player.
     """
+
+    # if a move causes the player to lose, the score should be the worst it can be
     if game.is_loser(player):
         return float("-inf")
 
+    # if a move causes the player to win, the score should be the worst it can be
     if game.is_winner(player):
         return float("inf")
 
+
+    # count moves for both players
     own_moves = len(game.get_legal_moves(player))
     opp_moves = len(game.get_legal_moves(game.get_opponent(player)))
+    # return difference between number of moves available, weighting more importance on the active player's moves to create a solid foundation
     return float(3 * own_moves - opp_moves)
 
 
@@ -192,24 +231,38 @@ class MinimaxPlayer(IsolationPlayer):
         return best_move
 
     def max_value(self, game, depth):
+        # check whether the timer has expired
         if self.time_left() < self.TIMER_THRESHOLD:
             raise SearchTimeout()
 
+        # define recursive end condition as whether there are no moves left to execute or we've hit the target depth
         if not bool(game.get_legal_moves()) or depth == 0: 
+            # if recursive end condition met, return the evaluation function of the current board state for the player
             return self.score(game, self)
+
+        # initialize the max to negative infinity so that it will get set to any other value
         max_val = float("-inf")
+        # call the min function for the next level of the tree for each move, decreasing the depth by one to signify going down the tree
         for move in game.get_legal_moves():
+            # retain the maximum value across all nodes analyzed
             max_val = max(max_val, self.min_value(game.forecast_move(move), depth - 1))
         return max_val
 
     def min_value(self, game, depth):
+        # check whether the timer has expired
         if self.time_left() < self.TIMER_THRESHOLD:
             raise SearchTimeout()
         
+        # define recursive end condition as whether there are no moves left to execute or we've hit the target depth
         if not bool(game.get_legal_moves()) or depth == 0:
+            # if recursive end condition met, return the evaluation function of the current board state for the player
             return self.score(game, self)
+        
+        # initialize the min to positive infinity so that it will get set to any other value
         min_val = float("inf")
+        # call the max function for the next level of the tree for each move, decreasing the depth by one to signify going down the tree
         for move in game.get_legal_moves():
+            # retain the minimum value across all nodes analyzed
             min_val = min(min_val, self.max_value(game.forecast_move(move), depth - 1))
         return min_val
 
@@ -252,13 +305,23 @@ class MinimaxPlayer(IsolationPlayer):
                 each helper function or else your agent will timeout during
                 testing.
         """
+        # check whether the timer has expired
         if self.time_left() < self.TIMER_THRESHOLD:
             raise SearchTimeout()
 
+        # initialize the value to negative infinity so that it will get set to any other value (since the starting player is max)
         best_val = float("-inf")
-        best_move = (-1, -1)
+        legal_moves = game.get_legal_moves()
 
-        for move in game.get_legal_moves():
+        # check to see if there are no moves available and return according to API
+        if not legal_moves:
+            return (-1, -1)
+        
+        # initialize a best move to one of the legal moves randomly in case the first ply evaluation times out before completing
+        best_move = legal_moves[random.randint(0, len(legal_moves) - 1)]
+
+        for move in legal_moves:
+            # kick off the minimax recursion and retain the maximum value and associated best move
             (best_val, best_move) = max((best_val, best_move), (self.min_value(game.forecast_move(move), depth - 1), move))
 
         return best_move
@@ -304,12 +367,20 @@ class AlphaBetaPlayer(IsolationPlayer):
 
         # Initialize the best move so that this function returns something
         # in case the search fails due to timeout
-        best_move = (-1, -1)
+        legal_moves = game.get_legal_moves()
+
+        # check to see if there are no moves available and return according to API
+        if not legal_moves:
+            return (-1, -1)
+        # initialize a best move to one of the legal moves randomly in case the first ply evaluation times out before completing
+        best_move = legal_moves[random.randint(0, len(legal_moves) - 1)]
 
         try:
             iterative_depth = 1
         # The try/except block will automatically catch the exception
         # raised when the timer is about to expire.
+
+        # evaluate increasing depths until the timer runs out
             while True:
                 best_move = self.alphabeta(game, iterative_depth)
                 iterative_depth += 1
@@ -320,30 +391,46 @@ class AlphaBetaPlayer(IsolationPlayer):
         return best_move
 
     def max_value(self, game, depth, alpha, beta):
+        # check whether the timer has expired
         if self.time_left() < self.TIMER_THRESHOLD:
             raise SearchTimeout()
 
+        # define recursive end condition as whether there are no moves left to execute or we've hit the target depth
         if not bool(game.get_legal_moves()) or depth == 0: 
+            # if recursive end condition met, return the evaluation function of the current board state for the player
             return self.score(game, self)
+        
+        # initialize the max to negative infinity so that it will get set to any other value
         max_val = float("-inf")
+        # call the min function for the next level of the tree for each move, decreasing the depth by one to signify going down the tree
         for move in game.get_legal_moves():
             max_val = max(max_val, self.min_value(game.forecast_move(move), depth - 1, alpha, beta))
+            # check to see if further evaluation is skippable by alpha-beta pruning and return if so
             if (max_val >= beta):
                 return max_val
+            # set alpha for future alpha-beta pruning in the min_value function
             alpha = max(alpha, max_val)
         return max_val
 
     def min_value(self, game, depth, alpha, beta):
+        # check whether the timer has expired
         if self.time_left() < self.TIMER_THRESHOLD:
             raise SearchTimeout()
         
+        # define recursive end condition as whether there are no moves left to execute or we've hit the target depth
         if not bool(game.get_legal_moves()) or depth == 0:
+            # if recursive end condition met, return the evaluation function of the current board state for the player
             return self.score(game, self)
+        
+        # initialize the min to positive infinity so that it will get set to any other value
         min_val = float("inf")
+        # call the min function for the next level of the tree for each move, decreasing the depth by one to signify going down the tree
         for move in game.get_legal_moves():
             min_val = min(min_val, self.max_value(game.forecast_move(move), depth - 1, alpha, beta))
+            # check to see if further evaluation is skippable by alpha-beta pruning and return if so
             if (min_val <= alpha):
                 return min_val
+            # set beta for future alpha-beta pruning in the max_value function
             beta = min(beta, min_val)
         return min_val
 
@@ -392,23 +479,32 @@ class AlphaBetaPlayer(IsolationPlayer):
                 each helper function or else your agent will timeout during
                 testing.
         """
+        # check whether the timer has expired
         if self.time_left() < self.TIMER_THRESHOLD:
             raise SearchTimeout()
 
+        # initialize the value to negative infinity so that it will get set to any other value (since the starting player is max)
         best_val = float("-inf")
         legal_moves = game.get_legal_moves()
+        # check to see if there are no moves available and return according to API
         if not legal_moves:
             return (-1, -1)
         
+        # initialize a best move to one of the legal moves randomly in case the first ply evaluation times out before completing
         best_move = legal_moves[random.randint(0, len(legal_moves) - 1)]
 
         for move in legal_moves:
+            # kick off the minimax with alpha-beta pruning recursion and retain the value
             val = max(self.min_value(game.forecast_move(move), depth - 1, alpha, beta), best_val)
+            # check to see if the value is a new max
             if (best_val < val):
+                # save the new highest value and associated move
                 best_val = val
                 best_move = move
+                # check to see if alpha-beta pruning can skip the rest of the evaluations and return the move if so
                 if (val >= beta):
                     return best_move
+            # save alpha for future alpha-beta pruning for the min_value function
             alpha = max(alpha, val)
 
         return best_move
